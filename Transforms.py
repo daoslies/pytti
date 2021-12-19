09120df02c4ac70ac2826ae6396112006c07387d
daoslies <54991131+daoslies@users.noreply.github.com>
2021-12-19 03:45:46 +0000

    Update Transforms.py

--- Transforms.py
+++ Transforms.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from PIL import Image, ImageFilter
 import numpy as np
 from pytti import *
-from pytti.LossAug.DepthLoss import DepthLoss
+from pytti5.LossAug.DepthLoss import DepthLoss
 from infer import InferenceHelper
 
 PADDING_MODES = {'mirror':'reflection','smear':'border','black':'zeros','wrap':'zeros'}
