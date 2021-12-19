30ac4ca009e65c456b81de36e79579f427b06908
daoslies <54991131+daoslies@users.noreply.github.com>
2021-12-19 03:40:54 +0000

    Update TVLoss.py

--- LossAug/TVLoss.py
+++ LossAug/TVLoss.py
@@ -1,6 +1,6 @@
 from torch.nn import functional as F
 import math
-from pytti.LossAug import Loss
+from pytti5.LossAug import Loss
 
 def tv_loss(input):
   """L2 total variation loss, as in Mahendran et al."""
